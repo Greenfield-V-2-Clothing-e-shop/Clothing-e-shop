ef87a48646cd46cfad42fc9db40394b2b63e3e9b
yahyadhaou <yahya.dhaou75@gmail.com>
2022-12-13 09:04:47 +0100

    done

---
+++ clothing-e-shop/component/layout.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Head from "next/head";
+import { AppBar,Toolbar, Typography } from "@mui/material";
+import { Container } from "@mui/system";
+
+
+export default function layout({children}){
+    return(
+<div>
+<Head>
+<title>
+    labasni
+</title>
+</Head>
+<AppBar position="static">
+<Toolbar>
+<Typography>labasni</Typography>
+</Toolbar>
+</AppBar>
+<Container>
+    {children}
+</Container>
+<footer>
+    <Typography>
+        all rights reserved labasni
+    </Typography>
+</footer>
+
+</div>
+ )
+}
