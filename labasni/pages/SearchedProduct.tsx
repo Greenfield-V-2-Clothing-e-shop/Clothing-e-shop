1e9f7aaa0a445993637cb0c11b099136aae4d835
MehdiDissem <mehdissem@gmail.com>
2022-12-14 15:26:47 +0100

    sidebar and buttons fixed

---
+++ labasni/pages/SearchedProduct.tsx
@@ -0,0 +1,3 @@
+export default function SearchedProduct() {
+  return <div>You are now on the search component</div>;
+}
--- labasni/pages/Sidebar.tsx
+++ labasni/pages/Sidebar.tsx
@@ -5,23 +5,36 @@ import Button from '@mui/material/Button';
 import List from '@mui/material/List';
 import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import TextField from '@mui/material/TextField';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Slider from '@mui/material/Slider';
+import {useRouter} from "next/router"
+import Link from "next/link"
+
 
 
 type Anchor = 'top' | 'left' | 'bottom' | 'right';
 
 export default function TemporaryDrawer() {
+  const router = useRouter();
   const [state, setState] = React.useState({
     top: false,
     left: false,
     bottom: false,
     right: false,
   });
+
+  const handleClick = (e, path) => { //hédhi béch témchi lél page search
+    if (path === "/SearchedProduct") {
+      console.log("I clicked on the About Page");
+    }
+  }
+
+  function valuetext(value: number) { //hédhi mta3 él price slide
+    return `${value}°C`;
+  }
 
   const toggleDrawer =
     (anchor: Anchor, open: boolean) =>
@@ -41,7 +54,7 @@ export default function TemporaryDrawer() {
     <Box
       sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
       role="presentation"
-      onClick={toggleDrawer(anchor, false)}
+      // onClick={toggleDrawer(anchor, false)}
       
     >
       <Box
@@ -52,22 +65,59 @@ export default function TemporaryDrawer() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <Button>Submit</Button>
+      <TextField id="outlined-basic" label="Search here" variant="outlined" />
     </Box>
 
       <List>
-        {['Search', 'Size', 'Color', 'Brand'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
+        
+          <ListItem disablePadding>
+          <FormGroup>
+            <h3>Sizes</h3>
+      <FormControlLabel control={<Checkbox />} label="S" />
+      <FormControlLabel control={<Checkbox />} label="M" />
+      <FormControlLabel control={<Checkbox />} label="L" />
+    </FormGroup>
           </ListItem>
-        ))}
+          <ListItem disablePadding>
+          <FormGroup>
+          <h3>Colors</h3>
+      <FormControlLabel control={<Checkbox />} label="Red" />
+      <FormControlLabel control={<Checkbox />} label="Blue" />
+      <FormControlLabel control={<Checkbox />} label="Green" />
+    </FormGroup>
+          </ListItem>
+          <ListItem disablePadding>
+          <FormGroup>
+          <h3>Brand</h3>
+      <FormControlLabel control={<Checkbox />} label="UnderArmour" />
+      <FormControlLabel control={<Checkbox />} label="Nike" />
+      <FormControlLabel control={<Checkbox />} label="Puma" />
+      <FormControlLabel control={<Checkbox />} label="MBA" />
+      <FormControlLabel control={<Checkbox />} label="MLBS" />
+      <FormControlLabel control={<Checkbox />} label="Football" />
+    </FormGroup>
+          </ListItem>
+          <ListItem disablePadding>
+          <FormGroup>
+          <h3>Price</h3>
+          <Slider sx={{width:200}}
+        aria-label="Temperature"
+        defaultValue={30}
+        getAriaValueText={valuetext}
+        valueLabelDisplay="auto"
+        step={10}
+        marks
+        min={10}
+        max={110}
+      />
+    </FormGroup>
+          </ListItem>
       </List>
+      <Button variant="contained" color="success">
+      <Link href="/SearchedProduct" onClick={(e) => handleClick(e, "/SearchedProduct")}>
+      Submit Search
+      </Link>{" "}
+</Button>
       <Divider />
     </Box>
   );
