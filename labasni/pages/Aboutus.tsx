7c271e1e8ef2826c8c90518d321ae4a5d4a802fa
yahyadhaou <yahya.dhaou75@gmail.com>
2022-12-15 15:08:09 +0100

    done

--- labasni/pages/Aboutus.tsx
+++ labasni/pages/Aboutus.tsx
@@ -1,5 +1,45 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 export default function Aboutus(){
   return <div>
-    This is about us page
+    <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="/Home">LABASNI</a> SPORTS SHOP
+        </h1>
+
+        <p className={styles.description}>
+          WE START WOKING ON LABASNI SINCE 2021
+          <code className={styles.code}></code>.
+        </p>
+
+       
+
+        <div className={styles.grid}>
+          <a href="#" className={styles.card}>
+            <h2>histroy &rarr;</h2>
+            <p>WE ARE IN THE E-COMMERCE SINCE 2020
+              AND LABASNI IS OUR THIRD PROJECT 
+            </p>
+          </a>
+
+         <div className={styles.card}>
+            <h2>OUR SERVICE &rarr;</h2>
+            <p>WE are presanting the high quallity 
+              and the offical product .<br></br>
+              we are the first 
+            </p>
+            </div>
+
+         <div className={styles.card}>
+            <h2>OUR BRANDS &rarr;</h2>
+            <p>NIKE ,UNDERARMOUR,JORDAN<br></br>PUMA,NBA,MLBS<br></br> FOOT JERESEY FOOT
+            </p>
+            </div>
+
+          
+        </div>
+      </main>
   </div>
 }
