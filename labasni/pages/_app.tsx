de643925e4724994af51d05f5b8cdea3342cc5b2
yahyadhaou <yahya.dhaou75@gmail.com>
2022-12-15 15:52:37 +0100

    done

--- labasni/pages/_app.tsx
+++ labasni/pages/_app.tsx
@@ -7,9 +7,10 @@ import PrivateRoute from "./privateRoute";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
-      
+      <PrivateRoute>
     <Navbar/>
     <Sidebar/>
+    </PrivateRoute>
   <Component {...pageProps} />
   </div>
   )
